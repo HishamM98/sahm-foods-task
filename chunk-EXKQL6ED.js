66ef1d9b53890f56390e05ad9d4aa36e43a76098
Hisham Zaitoun <hisham.mohammed98@yahoo.com>
2026-07-28 00:36:07 +0300

    Auto-generated commit

--- chunk-EXKQL6ED.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-EOULDKCD.js";var e=[{path:"",loadComponent:()=>import("./chunk-C64KWFUB.js").then(o=>o.LiveOrdersPage)},{path:"**",redirectTo:""}];export{e as ordersRoutes};
---
+++ chunk-HRECROUM.js
@@ -0,0 +1,1 @@
+import"./chunk-EOULDKCD.js";var e=[{path:"",loadComponent:()=>import("./chunk-4ZZQTJYA.js").then(o=>o.LiveOrdersPage)},{path:"**",redirectTo:""}];export{e as ordersRoutes};
