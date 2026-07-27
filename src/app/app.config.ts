59dcd2f584ce9dce7bac5f118cbacd93c7eaf050
Hisham Zaitoun <hisham.mohammed98@yahoo.com>
2026-07-28 00:21:28 +0300

    route hashing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideTranslateService } from '@ngx-translate/core';
@@ -17,7 +17,7 @@ registerLocaleData(ar);
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(withInterceptors([cacheInterceptor, errorInterceptor])),
     provideTranslateService({
       loader: provideTranslateHttpLoader({
